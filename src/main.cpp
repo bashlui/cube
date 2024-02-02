c8266c51a3737ced7de3e359bbbe5b6f3433c034
bashlui <luisboltech005@outlook.com>
2024-02-02 13:16:56 -0600

    Cube works lol

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,34 @@
 #include "screen.h"
 #include <numeric>
 
+struct vec3
+{
+    float x, y, z;
+};
+
+struct connection
+{
+    int a, b;
+};
+
+void rotate(vec3& point, float x = 1, float y = 1, float z = 1)
+{
+    float rad = 0;
+
+    rad = x;
+    point.y = std::cos(rad) * point.y - std::sin(rad) * point.z;
+    point.z = std::sin(rad) * point.y + std::cos(rad) * point.z;
+
+    rad = y;
+    point.x = std::cos(rad) * point.x + std::sin(rad) * point.z;
+    point.z = -std::sin(rad) * point.x + std::cos(rad) * point.z;
+
+    rad = z;
+    point.x = std::cos(rad) * point.x - std::sin(rad) * point.y;
+    point.y = std::sin(rad) * point.x + std::cos(rad) * point.y;
+}
+
+
 void line(Screen& screen, float x1, float y1, float x2, float y2)
 {
     float dx = x2 - x1;
@@ -22,20 +50,73 @@ int main()
 {
     Screen screen;
 
-    for (int i = 0; i < 100; i++)
+    std::vector<vec3> points
     {
-        screen.pixel(rand()%640, rand()%480);
+            {100, 100, 100},
+            {200, 100, 100},
+            {200, 200, 100},
+            {100, 200, 100},
+
+            {100, 100, 200},
+            {200, 100, 200},
+            {200, 200, 200},
+            {100, 200, 200}
+    };
+
+    std::vector<connection> connections{
+            {0, 4},
+            {1, 5},
+            {2, 6},
+            {3, 7},
+
+            {0, 1},
+            {1, 2},
+            {2, 3},
+            {3, 0},
+
+            {4, 5},
+            {5, 6},
+            {6, 7},
+            {7, 4}
+    };
+
+    vec3 c{0, 0, 0,};
+    for (auto& p : points)
+    {
+        c.x += p.x;
+        c.y += p.y;
+        c.z += p.z;
     }
 
-    line(screen, 100, 100, 200, 100);  // Top line
-    line(screen, 200, 100, 200, 200);  // Right line
-    line(screen, 200, 200, 100, 200);  // Bottom line
-    line(screen, 100, 200, 100, 100);  // Left line
+    c.x /= points.size();
+    c.y /= points.size();
+    c.z /= points.size();
 
     while(true)
     {
+        for(auto& p : points)
+        {
+            p.x -= c.x;
+            p.y -= c.y;
+            p.z -= c.z;
+            rotate(p, 0.02, 0.01, 0.04);
+            p.x += c.x;
+            p.y += c.y;
+            p.z += c.z;
+            screen.pixel(p.x, p.y);
+        }
+
+        for(auto& conn : connections)
+        {
+            line(screen, points[conn.a].x,
+                         points[conn.a].y,
+                         points[conn.b].x,
+                         points[conn.b].y);
+        }
         screen.show();
+        screen.clear();
         screen.input();
+        SDL_Delay(30);
     }
     return 0;
 }
